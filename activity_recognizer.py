6b7449a7c045c321c077b2e057c0e16db853b41a
Marcel Mozet <marcel.mozet@stud.uni-regensburg.de>
2026-05-16 11:09:03 +0200

    fix processing and continue recognizer

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -3,45 +3,85 @@ from file_reader import CSVReader
 from data_processing import DataPreprocessser
 from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
 from sklearn import svm
+from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
+
 
 TIME_WINDOW = 2
 
-strategies = {
+strategies = { # das hier wird eigentlich auch nicht gebracuht nur im notebook für testing
     "normal": lambda kernel: svm.SVC(kernel=kernel),
     "one_vs_one": lambda kernel: OneVsOneClassifier(svm.SVC(kernel=kernel)),
     "one_vs_rest": lambda kernel: OneVsRestClassifier(svm.SVC(kernel=kernel)),
 }
 
+kernels = {"linear", "poly", "rbf", "sigmoid"}
+
 classifiers = []
 predictions_list = []
+
 
 def data_setup(time_window):
     reader = CSVReader()
     recordings = reader.get_data_files_as_df()
     preprocessor = DataPreprocessser()
     preprocessor.convert_gyro_data(recordings)
-    
-    train_recordings, test_recordings = preprocessor.split_recordings_to_train_test(recordings)
+
+    train_recordings, test_recordings = preprocessor.split_recordings_to_train_test(
+        recordings
+    )
     # suggestion from ChatGPT to already split here since train and test data should be normalized
     # only with the scaler fit to the test data
-    
+
     train_windows = preprocessor.create_time_windows(train_recordings, time_window)
-    test_windows =  preprocessor.create_time_windows(test_recordings, time_window)
-    
-        
+    test_windows = preprocessor.create_time_windows(test_recordings, time_window)
+
     classifier_train_data = preprocessor.transform_windows_to_features(train_windows)
     classifier_test_data = preprocessor.transform_windows_to_features(test_windows)
-    
-    standard_scaled_train = preprocessor.perform_standard_scaling(classifier_train_data, classifier_train_data)
-    standard_scaled_test = preprocessor.perform_standard_scaling(classifier_train_data, classifier_test_data)
-    
-    normalized_train = preprocessor.perform_normalization(standard_scaled_train, standard_scaled_train )
-    normalized_test = preprocessor.perform_normalization(standard_scaled_train, standard_scaled_test)
 
-def model_train():
-    pass
-def capure_data_continuosly():
+    standard_scaled_train = preprocessor.perform_standard_scaling(
+        classifier_train_data, classifier_train_data
+    )
+    standard_scaled_test = preprocessor.perform_standard_scaling(
+        classifier_train_data, classifier_test_data
+    )
+
+    normalized_train = preprocessor.perform_normalization(
+        standard_scaled_train, standard_scaled_train
+    )
+    normalized_test = preprocessor.perform_normalization(
+        standard_scaled_train, standard_scaled_test
+    )
+
+    train_activities = normalized_train["activity"]
+    train_features = normalized_train.copy().drop(columns="activity")
+
+    test_activities = normalized_test["activity"]
+    test_features = normalized_test.copy().drop(columns="activity")
+
+    return train_activities, train_features, test_activities, test_features
+
+def train_and_evaluate(classifier, features_train, classes_train, features_test, classes_test): # function code from notebook by max
+    classifier.fit(features_train, classes_train)
+
+    classes_predicted = classifier.predict(features_test) #hier eigentlich doch predict auf aufgezeichnete daten
+     
+    accuracy = accuracy_score(classes_test, classes_predicted) # für was braucht man dann test hier? braucht man das im laufenden recogniizer?
+    return classes_predicted, accuracy
+
+def plot_classification_report(classes_test, classes_predicted): # fucntion code from notebook by max
+    report = classification_report(classes_test, classes_predicted, output_dict=True)
+    df = pd.DataFrame(report)
+    return df
+
+
+
+def capture_data_continously():
     pass
 
-data_setup(TIME_WINDOW)
- 
+
+train_labels, train_features, test_labels, test_features = data_setup(TIME_WINDOW)
+
+classifier = function(kernels[0])
+classifiers.append(classifier)
+predictions, accuracy = train_and_evaluate(classifier, train_features, train_labels, test_features, train_labels)
--- data_processing.py
+++ data_processing.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from collections import Counter
 import re
 
+
 class DataPreprocessser:
     def __init__(self):
         pass
@@ -18,10 +19,9 @@ class DataPreprocessser:
                 recording["data"]["gyro_y"] = np.deg2rad(recording["data"]["gyro_y"])
                 recording["data"]["gyro_z"] = np.deg2rad(recording["data"]["gyro_z"])
 
-
-    def split_recordings_to_train_test(whole_set, recordings):
+    def split_recordings_to_train_test(self, recordings):
         training, test = train_test_split(
-            whole_set,
+            recordings,
             test_size=0.2,
             stratify=[
                 f"{r['activity']}_{r['name']}_{r['sensor_placement']}_{r['sample_rate']}"
@@ -29,7 +29,6 @@ class DataPreprocessser:
             ],
         )
         return training, test
-
 
     def create_time_windows(self, recordings, window_length):
         windows = []
@@ -53,15 +52,14 @@ class DataPreprocessser:
 
         return windows
 
-
     def transform_windows_to_features(self, windows):
         feature_rows = []
-        
+
         for window in windows:
             feature_row = {}
-            
+
             feature_row["activity"] = window["activity"].iloc[0]
-            
+
             for acc_col in ["acc_x", "acc_y", "acc_z"]:
                 feature_row[f"{acc_col}_mean"] = window[acc_col].mean()
                 feature_row[f"{acc_col}_std"] = window[acc_col].std()
@@ -74,58 +72,56 @@ class DataPreprocessser:
                 feature_row[f"{gyro_col}_min"] = window[gyro_col].min()
                 feature_row[f"{gyro_col}_max"] = window[gyro_col].max()
 
-            acc_strengths = np.sqrt(window["acc_x"]**2 + window["acc_y"]**2 + window["acc_z"]**2)
-            gyro_strengths = np.sqrt(window["gyro_x"]**2 + window["gyro_y"]**2 + window["gyro_z"]**2)
-            
+            acc_strengths = np.sqrt(
+                window["acc_x"] ** 2 + window["acc_y"] ** 2 + window["acc_z"] ** 2
+            )
+            gyro_strengths = np.sqrt(
+                window["gyro_x"] ** 2 + window["gyro_y"] ** 2 + window["gyro_z"] ** 2
+            )
+
             feature_row["acc_strenght_mean"] = acc_strengths.mean()
             feature_row["acc_strenght_std"] = acc_strengths.std()
-            
+
             feature_row["gyro_strenght_mean"] = gyro_strengths.mean()
             feature_row["gyro_strenght_std"] = gyro_strengths.std()
-            
+
             acc_strengths_hamming = acc_strengths * np.hamming(len(acc_strengths))
             acc_fft = np.fft.rfft(acc_strengths_hamming)
-            acc_freqs = np.fft.rfftfreq(len(acc_strengths_hamming), 1/len(window)) 
+            acc_freqs = np.fft.rfftfreq(len(acc_strengths_hamming), 1 / len(window))
             feature_row["acc_dom_freq"] = acc_freqs[np.argmax(np.abs(acc_fft))]
-            
-            
+
             gyro_strengths_hamming = gyro_strengths * np.hamming(len(gyro_strengths))
             gyro_fft = np.fft.rfft(gyro_strengths_hamming)
-            gyro_freqs = np.fft.rfftfreq(len(gyro_strengths_hamming), 1/len(window)) 
+            gyro_freqs = np.fft.rfftfreq(len(gyro_strengths_hamming), 1 / len(window))
             feature_row["dom_gyro_freq"] = gyro_freqs[np.argmax(np.abs(gyro_fft))]
-            
+
             feature_rows.append(feature_row)
-        
+
         classifier_data = pd.DataFrame(feature_rows)
         return classifier_data
-
 
     def perform_standard_scaling(self, train_df, df_to_scale):
         scaler = StandardScaler()
         scaler.fit(train_df[[col for col in train_df.columns if col != "activity"]])
-        scaled_samples =  scaler.transform(df_to_scale[[col for col in df_to_scale.columns if col != "activity"]])
+        scaled_samples = scaler.transform(
+            df_to_scale[[col for col in df_to_scale.columns if col != "activity"]]
+        )
         df_scaled = df_to_scale.copy()
-        df_scaled[[col for col in df_scaled.columns if col != "activity"]] = scaled_samples
+        df_scaled[[col for col in df_scaled.columns if col != "activity"]] = (
+            scaled_samples
+        )
         return df_scaled
 
     def perform_normalization(self, train_df, df_to_normalize):
         scaler = MinMaxScaler()
         scaler.fit(train_df[[col for col in train_df.columns if col != "activity"]])
-        scaled_samples = scaler.transform(df_to_normalize[[col for col in df_to_normalize.columns if col != "activity"]])
+        scaled_samples = scaler.transform(
+            df_to_normalize[
+                [col for col in df_to_normalize.columns if col != "activity"]
+            ]
+        )
         df_normalized = df_to_normalize.copy()
-        df_normalized[[col for col in df_normalized.columns if col != "activity"]] = scaled_samples
+        df_normalized[[col for col in df_normalized.columns if col != "activity"]] = (
+            scaled_samples
+        )
         return df_normalized
-
-
-
-classifier_train_data = transform_windows_to_features(train_windows)
-classifier_test_data = transform_windows_to_features(test_windows)
-
-standard_scaled_train = perform_standard_scaling(classifier_train_data, classifier_train_data)
-normalized_train = perform_normalization(standard_scaled_train, standard_scaled_train )
-
-standard_scaled_test = perform_standard_scaling(classifier_train_data, classifier_test_data)
-normalized_test = perform_normalization(standard_scaled_train, standard_scaled_test)
-
-
-# classifier mit normalized_train und normalized_test trainieren usw
